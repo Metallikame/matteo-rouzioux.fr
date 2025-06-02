730907d5970a6f755c67d9e244ad033de98dc084
metal <rex2644@outlook.com>
2025-06-02 16:56:54 +0200

    Commit initial projet react

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => (
+  <h1>Hello React + Vite + TypeScript!</h1>
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
