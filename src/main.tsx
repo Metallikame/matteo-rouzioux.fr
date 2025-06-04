00ae7527ccaf754d9eb20416a477658b6caa4e17
metal <rex2644@outlook.com>
2025-06-04 22:02:11 +0200

    test de mise à jour

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => <h1>Hello React + Vite + TypeScript!</h1>;
+const App = () => <h1>Coucou c'est mon site à moua ! 🤡🤡</h1>;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
