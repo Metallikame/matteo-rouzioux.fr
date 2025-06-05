98663eb55e80c78b9f2510a64560b621be9a8125
metal <rex2644@outlook.com>
2025-06-05 23:15:46 +0200

    Mise au propre sonarqube

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => (
-  <div style={{ textAlign: "center", marginTop: "3rem" }}>
+  <div style={{ textAlign: 'center', marginTop: '3rem' }}>
     <h1>🚧 Site actuellement en construction ! 🚧</h1>
-    <div id="animation-container" lang="en" role="img" className="main" aria-label="Lottie animation">
-      <div id="animation" className="animation">
-        <img
-          src="/gif chantier.svg"
-          alt="Chantier en construction"
-          style={{ width: "250px", marginTop: "2rem" }}
-        />
-      </div>
-    </div>
+    <img
+      src="/gif chantier.svg"
+      alt="Chantier en construction"
+      style={{ width: '250px', marginTop: '2rem' }}
+    />
   </div>
 );
 
