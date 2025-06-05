45f77d66caa77b40172b4bf654c559804501a47c
metal <rex2644@outlook.com>
2025-06-05 23:09:02 +0200

    Ajout final de mise en construction

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => <h1>🚧Site actuellement en construction !🚧</h1>;
+const App = () => (
+  <div style={{ textAlign: "center", marginTop: "3rem" }}>
+    <h1>🚧 Site actuellement en construction ! 🚧</h1>
+    <div id="animation-container" lang="en" role="img" className="main" aria-label="Lottie animation">
+      <div id="animation" className="animation">
+        <img
+          src="/gif chantier.svg"
+          alt="Chantier en construction"
+          style={{ width: "250px", marginTop: "2rem" }}
+        />
+      </div>
+    </div>
+  </div>
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
